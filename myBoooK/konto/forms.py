aebe16668a144205d0d741b450c98693e1f0e1cf
magic075 <magic07589@gmail.com>
2021-01-10 14:28:26 +0100

    edit forms edit models no tested

--- myBoooK/konto/forms.py
+++ myBoooK/konto/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LogowanieForm(forms.Form):
@@ -20,3 +21,15 @@ class RejestracjaUzytkownika(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Niestety hasła nie są takie same!!")
         return cd['password2']
+
+
+class FormularzEdycjiUzytkownika(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class FormularzEdycjiProfilu(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('city', 'date_of_birth','phoneNumber ', 'photo')
+
--- myBoooK/konto/models.py
+++ myBoooK/konto/models.py
@@ -7,6 +7,9 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField()
+    city = models.CharField(max_length=99, default='')
+    phoneNumber = models.IntegerField(default=0)
+    email = models.EmailField(_('email address'), blank=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
 
     def __str__(self):
