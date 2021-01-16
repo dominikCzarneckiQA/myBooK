29ebe93ed71acd50c6ff783c0b354563824c2688
magic075 <magic07589@gmail.com>
2021-01-16 19:51:36 +0100

    add some bootstrap

--- myBoooK/myBoooK/settings.py
+++ myBoooK/myBoooK/settings.py
@@ -116,15 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/myBoooK/konto/templates/registration/../konto/static/css"
+STATIC_ROOT = "/myBoooK/konto/static"
 LOGIN_URL = '/konto/login/'
 
 
 # Login/ Logout / dashboard przekierowania
-
 LOGIN_REDIRECT_URL = 'dashboard'
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
+
+
 
 # SERWER SMTP
 EMAIL_HOST = "smtp.gmail.com"
