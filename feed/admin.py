30916fd5da2e1f2850228d3125d0fc9a9290793f
magic075 <magic07589@gmail.com>
2021-01-18 19:48:14 +0100

    eidt admin.py register model Post  in admin

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug' ,'img' , 'creation_date']
+    list_filter = ['creation_date']
