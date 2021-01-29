f1566cafd85625f094747a707d518376d75d44ff
magic075 <magic07589@gmail.com>
2021-01-29 03:32:38 +0100

    Social Network App myBooK create new model, create new profile view, add url profile, add some boots

--- konto/models.py
+++ konto/models.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,  primary_key=True, verbose_name='user',
-                                related_name='profile', on_delete=models.CASCADE, )
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,  primary_key=True, on_delete=models.CASCADE, )
     biography = models.TextField(max_length=399, null=True, blank=True)
     profileAvatar = models.ImageField(upload_to='profilePhoto', default='profilePhoto/default.jpg', blank=True)
     birthDate = models.DateField(null=True, blank=True, )
