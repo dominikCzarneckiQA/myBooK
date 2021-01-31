17800609b5ab13bdb6968e286c785de25c7c849b
magic075 <magic07589@gmail.com>
2021-01-31 21:43:40 +0100

    decorating the app

--- feed/views.py
+++ feed/views.py
@@ -13,59 +13,59 @@ from django.http import HttpResponseRedirect
 @method_decorator(login_required, name='dispatch')
 class AllPostView(View):
     def post(self, request, *args, **kwargs):
-        form = PostCreateForm(request.POST)
+        formAllPost = PostCreateForm(request.POST)
         allPosts = Post.objects.all().order_by('-postDate')
 
-        if form.is_valid():
-            newPost = form.save(commit=False)
+        if formAllPost.is_valid():
+            newPost = formAllPost.save(commit=False)
             newPost.postAuthor = request.user
             newPost.save()
             return HttpResponseRedirect(reverse_lazy('feed:all-posts'))
 
         return render(request, 'feed/allPost.html', {
             'allPosts': allPosts,
-            'form': form
+            'form': formAllPost
         })
 
     def get(self, request, *args, **kwargs):
-        form = PostCreateForm()
+        formAllPost = PostCreateForm()
         allPosts = Post.objects.all().order_by('-postDate')
 
         return render(request, 'feed/allPost.html', {
             'allPosts': allPosts,
-            'form': form
+            'form': formAllPost
         })
 
 
 @method_decorator(login_required, name='dispatch')
 class DetailPostView(View):
     def get(self, request, pk, *args, **kwargs):
-        post = Post.objects.get(pk=pk)
-        form = CommentCreateForm()
+        postget = Post.objects.get(pk=pk)
+        formDetail = CommentCreateForm()
 
-        allComments = Comment.objects.filter(post=post).order_by('-commentDate')
+        allComments = Comment.objects.filter(post=postget).order_by('-commentDate')
 
         return render(request, 'feed/detailPost.html', {
-            'post': post,
-            'form': form,
+            'post': postget,
+            'form': formDetail,
             'comments': allComments,
         })
 
     def post(self, request, pk, *args, **kwargs):
-        post = Post.objects.get(pk=pk)
-        form = CommentCreateForm(request.POST)
+        postget = Post.objects.get(pk=pk)
+        formDetail = CommentCreateForm(request.POST)
 
-        if form.is_valid():
-            newComment = form.save(commit=False)
+        if formDetail.is_valid():
+            newComment = formDetail.save(commit=False)
             newComment.commentAuthor = request.user
-            newComment.post = post
+            newComment.post = postget
             newComment.save()
 
-        allComments = Comment.objects.filter(post=post).order_by('-commentDate')
+        allComments = Comment.objects.filter(post=postget).order_by('-commentDate')
 
         return render(request, 'feed/detailPost.html', {
-            'post': post,
-            'form': form,
+            'post': postget,
+            'form': formDetail,
             'comments': allComments,
         })
 
@@ -102,4 +102,4 @@ class DeleteCommentView(UserPassesTestMixin, DeleteView):
         return self.request.user == self.get_object().commentAuthor
 
     def get_success_url(self):
-        return reverse_lazy('feed:detail-post', kwargs={'pk': self.kwargs['postPK']})
+        return reverse_lazy('feed:detail-post', kwargs={'pk': self.kwargs['post_pk']})
